bd3d9616fdaec2d8f8cf6c758fea982ca57a764e
PramodiPeshila <ppeshila7@gmail.com>
2025-02-26 09:58:20 +0530

    removed Oder menu part from the home page

--- Restuarant Landing Page/src/Components/Home.js
+++ Restuarant Landing Page/src/Components/Home.js
@@ -1,95 +1,44 @@
-import React ,{useState,useEffect}from "react";
+import React, { useState } from "react";
 import BannerBackground from "../Assest/home-banner-background.png";
 import BannerImage from "../Assest/home-banner-image.jpg";
-import './Home.css'
-import { FiArrowRight } from "react-icons/fi";
-
+import './Home.css';
+//import { FiArrowRight } from "react-icons/fi";
 
 const Home = () => {
   const [showMessage, setShowMessage] = useState(false);
-  const [welcomeMessage, setWelcomeMessage] = useState("Welcome!");
-  const [userName, setUserName] = useState("");//
-  const [dateTime, setDateTime] = useState(new Date());
-
-        useEffect(() => {
-            const timer = setInterval(() => {
-            setDateTime(new Date());
-            }, 1000);
-
-            
-            return () => clearInterval(timer);
-        }, []);
-
-            const getGreetingMessage = () => {
-            const hours = dateTime.getHours();
-            if (hours < 12) {
-            return "Good Morning..!";
-            } else if (hours < 18) {
-            return "Good Afternoon..!";
-            } else {
-            return "Good Evening..!";
-            }
-        };
-
+  
   const handleButtonClick = () => {
     setShowMessage(true);
   };
 
-  const handleInputChange = (event) => {
-    const name = event.target.value;
-    setUserName(name);
-    setWelcomeMessage(`Welcome, ${name} !`);
-  };
-
   return (
-    <div >
+    <div>
       <div className="home-banner-container">
-        < div className="home-bannerImage-container">
+        <div className="home-bannerImage-container">
           <img src={BannerBackground} alt="" />
         </div>
 
         <div className="home-text-section">
-          <div className="primary-heading">
-
-            <h2>{getGreetingMessage()} </h2>
-            <h3>{welcomeMessage}</h3>
-
-            <input 
-            type="text" 
-            placeholder="Enter your name" 
-            value={userName} 
-            onChange={handleInputChange} 
-            className="name-input"
-            />
-          </div>
-          
-
-          <br/><br/><br/>
-          <h1>Your Favourite Food Delivered Hot & Fresh</h1> 
+          <h1>Your Favourite Food Delivered Hot & Fresh</h1>
           
           <p className="primary-text">
-            Hurry Up!
-             <br/>Order from your nearest restuarant.   
+          Delicious meals, fresh ingredients, and unforgettable flavors. Dine in, take out, or get it delivered hot & fresh!
           </p>
           
           <button className="secondary-button" onClick={handleButtonClick}>
-            Order Now <FiArrowRight />
+            {/* Order Now <FiArrowRight /> */}
+            View Menu
           </button>
 
-          {showMessage && <p className="order-message">Select Menu</p>}
-
+          {showMessage && <p className="order-message"></p>}
         </div>
 
         <div className="home-image-section">
           <img src={BannerImage} alt="" />
         </div>
-
       </div>
     </div>
-
-
   );
 };
 
 export default Home;
-
