ac5a3c39bdc3e1e0dfd3dea3861b66b7d6a2dc96
PramodiPeshila <ppeshila7@gmail.com>
2025-02-26 22:41:13 +0530

    Modified Home.js

--- Restuarant Landing Page/src/Components/Home.js
+++ Restuarant Landing Page/src/Components/Home.js
@@ -1,8 +1,12 @@
 import React, { useState } from "react";
 import BannerBackground from "../Assest/home-banner-background.png";
 import BannerImage from "../Assest/home-banner-image.jpg";
+import breakfast from "../Assest/breakfast.jpg";
+import lunch from "../Assest/lunch.jpg";
+import dinner from "../Assest/dinner.jpg";
+
 import './Home.css';
-//import { FiArrowRight } from "react-icons/fi";
+
 
 const Home = () => {
   const [showMessage, setShowMessage] = useState(false);
@@ -22,11 +26,10 @@ const Home = () => {
           <h1>Your Favourite Food Delivered Hot & Fresh</h1>
           
           <p className="primary-text">
-          Delicious meals, fresh ingredients, and unforgettable flavors. Dine in, take out, or get it delivered hot & fresh!
+            Delicious meals, fresh ingredients, and unforgettable flavors. Dine in, take out, or get it delivered hot & fresh!
           </p>
           
           <button className="secondary-button" onClick={handleButtonClick}>
-            {/* Order Now <FiArrowRight /> */}
             View Menu
           </button>
 
@@ -37,6 +40,45 @@ const Home = () => {
           <img src={BannerImage} alt="" />
         </div>
       </div>
+
+      {/* New Section */}
+      <div className="meal-section">
+        <div className="meal-section-container">
+        <h1>We’re always here for you</h1>
+        <div className="meal-cards-container">
+          <div className="meal-card">
+            <img src={breakfast} alt="Breakfast Cravings" />
+            <h3>Breakfast Cravings</h3>
+            <p>
+              <span role="img" aria-label="Light meal">🥗</span> Light meal
+              <span role="img" aria-label="Fruit drink">🍹</span> Fruit drink
+              <span role="img" aria-label="From 7am">⏰</span> From 7am
+            </p>
+
+          </div>
+          
+          <div className="meal-card">
+            <img src={lunch} alt="Lunch Blast" />
+            <h3>Lunch Blast</h3>
+            <p>
+                <span role="img" aria-label="Spicy meal">🌶</span> Spicy meal
+                <span role="img" aria-label="Beverage">🍵</span> Beverage
+                <span role="img" aria-label="From 1pm">⏰</span> From 1pm
+            </p>
+          </div>
+          
+          <div className="meal-card">
+            <img src={dinner} alt="Dinner Madness" />
+            <h3>Dinner Madness</h3>
+            <p>
+                <span role="img" aria-label="Light meal">🥗</span> Light meal
+                <span role="img" aria-label="Beverage">🍷</span> Beverage
+                <span role="img" aria-label="From 9pm">⏰</span> From 9pm
+            </p>
+           </div>
+        </div>
+        </div>
+        </div>
     </div>
   );
 };
